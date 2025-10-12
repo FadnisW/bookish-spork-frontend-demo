e999fb2e9334a179487a3ea8144b6f81bddb1e59
FadnisW <fadnisw@gmail.com>
2025-10-13 01:56:45 +0530

    Solved the issue with favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Waqar Education Management Dashboard",
   description: "Next.js Education Management System",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({

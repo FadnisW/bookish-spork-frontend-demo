b83fd5f75146d53c5dbcdc91323dbfb7ee44d5a0
FadnisW <fadnisw@gmail.com>
2025-10-12 17:53:13 +0530

    Third Commit, completed the dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -10,7 +11,7 @@ export default function DashboardLayout({
   return (
       <div className="h-screen flex">
         {/* left */}
-        <div className="h-full w-[21%] md:w-[15%] lg:w-[20%] xl:w-[18%] bg-white-50 p-4 overflow-hidden flex flex-col">
+        <div className="h-full w-[21%] md:w-[15%] lg:w-[20%] xl:w-[18%] p-4 overflow-hidden flex flex-col">
           <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
             <Image src='/logo.png' alt='logo' width={32} height={32}/>
             <span className="hidden lg:block">bookish-spork</span>
@@ -18,7 +19,10 @@ export default function DashboardLayout({
           <Menu />
         </div>
         {/* right */}
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">right</div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+          <Navbar />
+          {children}
+        </div>
       </div>
   );
 }
